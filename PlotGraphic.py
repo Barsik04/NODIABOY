c5584bab422d33e0c280ade89c9792001ec0f970
prest <prestigio924@gmail.com>
2022-12-16 02:23:14 +0300

    Code Refactor

--- PlotGraphic.py
+++ PlotGraphic.py
@@ -8,38 +8,43 @@ from matplotlib import pyplot as plt
 class PlotGraphic():
 
     def plot_price(excel_data_df, reccomended_price, header):
-
-        fig, axs = plt.subplots(2, 1, constrained_layout=True)
-
+        plt.rcParams['figure.dpi'] = 250
+        plt.rcParams['savefig.dpi'] = 250
         sns.histplot(excel_data_df['Цена'].dropna(), bins=1000, color='purple')
-        #axs[0][0].set_suptitle(header, fontsize=12, color='black')
+        plt.suptitle(header, fontsize=12, color='black')
         min = excel_data_df["Цена"].min() * 0.9
         max = excel_data_df["Цена"].mean() * 2
-        #axs[0][0].set_axvline(reccomended_price, 0, excel_data_df['Цена'].dropna().max(), color = 'red', linewidth=2, label= 'РРЦ на момент продажи')
-        #axs[0][0].set_minorticks_on()
-        #axs[0][0].set_legend(loc=0)
-        axs[0][0].set_ylabel('Количество')
-        #axs[0][0].set_xlim(min, max)
-        axs[0][0].set_grid()
-
-
-
-
-
-
-
-        sns.histplot(excel_data_df['Цена'].dropna(), bins=1000, color='purple')
-        #axs[1][0].suptitle(header, fontsize=12, color='black')
-        min = excel_data_df["Цена"].mean() * 2
-        max = excel_data_df["Цена"].max()
-       # axs[1][0].set_axvline(reccomended_price, 0, excel_data_df['Цена'].dropna().max(), color='red', linewidth=2,                  #  label='РРЦ на момент продажи')
-        #axs[1][0].set_minorticks_on()
-        #axs[1][0].set_legend(loc=0)
-        axs[1][0].set_ylabel('Количество')
-        #axs[1][0].set_xlim(min, max)
-        #axs[1][0].grid()
-
+        plt.axvline(reccomended_price, 0, excel_data_df['Цена'].dropna().max(), color = 'red', linewidth=2, label= 'РРЦ на момент продажи')
         plt.minorticks_on()
+        plt.legend(loc=0)
+        plt.ylabel('Количество')
+        plt.xlim(min, max)
+        plt.grid(True)
+        plt.savefig('./output/by_price' + header)
+
+
+        sns.histplot(excel_data_df['Цена'].dropna(), bins=50, color='purple')
+        plt.suptitle(header, fontsize=12, color='black')
+        min = excel_data_df["Цена"].min() * 0.9
+        max = excel_data_df["Цена"].mean() * 2
+        plt.axvline(reccomended_price, 0, excel_data_df['Цена'].dropna().max(), color='red', linewidth=2,
+                    label='РРЦ на момент продажи')
+        plt.minorticks_on()
+        plt.legend(loc=0)
+        plt.ylabel('Количество')
+        plt.xlim(max, )
+        plt.ticklabel_format(style='plain')
+        plt.ylim(0, 10)
+        plt.grid(True)
+        plt.savefig('./output/by_price' + header + ' second_part')
         plt.show()
 
+    def plot_pie(excel_data_df, header):
+        city_data = excel_data_df['Город'].value_counts()
+        city_data = city_data.T
+        df = city_data.to_frame(city_data)
+        #city_data  = city_data.DataFrame(city_data, columns = ['city,' 'count'])
+        print (df.valuetolist())
+        #plot = excel_data_df.plot.pie(y = 'city', figsize=(5, 5))
+        plot.show()
 
--- ReadAndAnalyzeData.py
+++ ReadAndAnalyzeData.py
@@ -1,7 +1,7 @@
 import pandas
 from PlotGraphic import PlotGraphic
 
-file_name = 'iphone 14 128'
+file_name = 'iphone 11 64'
 reccomended_price = 60000
 
 excel_data_df = pandas.read_excel(file_name + '.xlsx')
@@ -9,11 +9,15 @@ excel_data_df = pandas.read_excel(file_name + '.xlsx')
 print(len(excel_data_df.index))
 excel_data_df = excel_data_df.drop_duplicates(subset='Ссылка')
 
-PlotGraphic.plot_price(excel_data_df, reccomended_price, 'График распределения до чистки данных для ' + file_name)
+#PlotGraphic.plot_price(excel_data_df, reccomended_price, 'График распределения цен  до чистки данных для ' + file_name)
+PlotGraphic.plot_pie(excel_data_df,  'График распределения после чистки данных по городам для ' + file_name + ' без сервисов' )
 
 excel_data_df = excel_data_df[excel_data_df["Наименование"].str.contains("pro") == False]
 excel_data_df = excel_data_df[excel_data_df["Наименование"].str.contains("Сбер") == False]
 excel_data_df = excel_data_df[excel_data_df["Наименование"].str.contains("альфа") == False]
+
 print(len(excel_data_df.index))
 
-PlotGraphic.plot_price(excel_data_df, reccomended_price, 'График распределения до чистки данных для ' + file_name + ' без сервисов' )
+#PlotGraphic.plot_price(excel_data_df, reccomended_price, 'График распределения цен до чистки данных для ' + file_name + ' без сервисов' )
+
+
